d08e089c627e5fefe0383eedb99eb611e1968393
Ian <ian.brummel@gmail.com>
2020-08-21 15:36:30 -0400

    Added convenience interfacing functions for the OmegaPlatinumController object and supporting enumerations.

--- Omega_Platinum.py
+++ Omega_Platinum.py
@@ -1,17 +1,18 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import minimalmodbus_OmegaPt as modbus
-from time import sleep
+import Omega_Platinum_Enum as ENUM
+import numpy as np
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtCore import pyqtSignal
 
 
 class OmegaPlatinumControllerModbus(QWidget):
-    data_ready = pyqtSignal()
-    def __init__(self, comport: str):
+    temp_ready = pyqtSignal()
+    def __init__(self, comport: str, rounding=2):
         super().__init__()
 
         try:
-            self.instrument = modbus.Instrument(comport, 1, mode='MODE_RTU', close_port_after_each_call=False,
+            self.controller = modbus.Instrument(comport, 1, mode='MODE_RTU', close_port_after_each_call=False,
                                                 debug=False)
         except Exception as err:
             print(err)
@@ -19,14 +20,70 @@ class OmegaPlatinumControllerModbus(QWidget):
             print("Could not connect to furnace controller, make sure all other control software is closed and you have"
                   " supplied the correct comport (Port Supplied='{}')".format(comport))
 
-        self.instrument.read_register(628)
+        self._SIGFIGS_AFTER_DECIMAL = rounding
 
+    def read_process_temp(self):
+        return np.round(float(self.controller.read_float(640)), self._SIGFIGS_AFTER_DECIMAL)
 
+    def read_system_state(self):
+        return ENUM.read.system_state[self.controller.read_register(576)]
 
-def print_hi():
-    pass
+    def set_system_state(self, state):
+        if state not in ENUM.write.system_state:
+            raise ValueError("Cannot set to invalid system state: {}".format(state))
+        self.controller.write_register(576, ENUM.write.system_state[state])
 
+    def read_tc_type(self):
+        return ENUM.read.tc_type[self.controller.read_register(643)]
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    def set_tc_type(self, tc_type):
+        if tc_type not in ENUM.write.tc_type:
+            raise ValueError("Cannot set thermocouple type to un-enumerated type: {}".format(tc_type))
+        self.controller.write_register(643, ENUM.write.tc_type[tc_type])
+
+    def read_output_mode(self, output_num: int):
+        if 1 <= output_num < 8:
+            datum = 'mode'
+            register = ENUM.register_map.get_output_register(output_num, datum)
+            return ENUM.read.output_mode[self.controller.read_register(register)]
+
+    def set_output_mode(self, output_num: int, mode: str):
+        if 1 <= output_num <= 8:
+            datum = 'mode'
+            register = ENUM.register_map.get_output_register(output_num, datum)
+            if mode not in ENUM.write.output_mode:
+                raise ValueError("Invalid output mode supplied {}. "
+                                 "Valid modes are {}".format(mode, ENUM.write.output_mode.keys()))
+            self.controller.write_register(register, ENUM.write.output_mode[mode])
+
+    def set_retransmission_trim(self, output_num: int, reading1=0., output1=0.,
+                                reading2=100., output2=1.):
+        if 1<=output_num<= 8:
+            data = ['retran_reading1', 'retran_output1', 'retran_reading2', 'retran_output2']
+            trims = [reading1, output1, reading2, output2]
+            if not (isinstance(reading1, (int, float)) and isinstance(output1, (int, float))
+                    and isinstance(reading2, (int, float)) and isinstance(output2, (int, float))):
+                raise TypeError('Retransmission output trim must be set using floats or ints. The following types were'
+                                ' supplied {}'.format([type(reading1), type(output1), type(reading2), type(output2)]))
+
+            for i, datum in enumerate(data):
+                register = ENUM.register_map.get_output_register(output_num, datum)
+                self.controller.write_float(register, trims[i])
+
+    def get_retransmission_trim(self, output_num: int):
+        if 1<=output_num<= 8:
+            data = ['retran_reading1', 'retran_output1', 'retran_reading2', 'retran_output2']
+            trims = []
+
+            for i, datum in enumerate(data):
+                register = ENUM.register_map.get_output_register(output_num, datum)
+                trims.append(float(self.controller.read_float(register)))
+
+            return trims
+
+    def set_ramp_soak_tracking(self, mode: str):
+        if mode not in ENUM.write.ramp_soak_tracking:
+            raise ValueError("Invalid ramp/soak tracking mode supplied: {}".format(mode))
+        self.controller.write_register(615, ENUM.write.ramp_soak_tracking[mode])
+
+    # TODO: Add profile editing and selection
--- Omega_Platinum_Enum.py
+++ Omega_Platinum_Enum.py
@@ -1,35 +1,98 @@
-control = {0: 'Stop',
-           1: 'Start',
-           2: 'Cancel',
-           3: 'Auto On',
-           4: 'Continuous'}
+class read():
+    control = {0: 'Stop',
+               1: 'Start',
+               2: 'Cancel',
+               3: 'Auto On',
+               4: 'Continuous'}
 
-control_action = {0: 'Reverse', 1: 'Direct'}
+    control_action = {0: 'Reverse', 1: 'Direct'}
 
-system_state = {0: 'Load', 1: 'Idle', 2: 'Input Adjust',
-                3: 'Control Adjust', 4: 'Modify', 5: 'Wait',
-                6: 'Run', 7: 'Standby', 8: 'Stop', 9: 'Pause',
-                10: 'Fault', 11: 'Shutdown', 12: 'Autotune'}
+    system_state = {0: 'Load', 1: 'Idle', 2: 'Input Adjust',
+                    3: 'Control Adjust', 4: 'Modify', 5: 'Wait',
+                    6: 'Run', 7: 'Standby', 8: 'Stop', 9: 'Pause',
+                    10: 'Fault', 11: 'Shutdown', 12: 'Autotune'}
 
-ramp_soak_state = {0x00: 'Inactive', 0x01: 'Ramping', 0x02: 'Soaking', 0x04: 'Ramp Active',
-                   0x08: 'Soak Active', 0x10: 'Ramp Soak Paused', 0x80: 'Ramp Soak Error'}
+    ramp_soak_tracking = {0: 'Ramp', 1: 'Soak', 2: 'Cycle'}
 
-ramp_soak_tracking = {0: 'Ramp', 1: 'Soak', 2: 'Cycle'}
+    sensor_type = {0: 'Thermocouple',
+                   1: 'RTD',
+                   2: 'Process Input',
+                   3: 'Thermistor',
+                   4: 'Remote'}
 
-sensor_type = {0: 'Thermocouple',
-               1: 'RTD',
-               2: 'Process Input',
-               3: 'Thermistor',
-               4: 'Remote'}
+    tc_type = {0: 'J', 1: 'K', 2: 'T',
+               3: 'E', 4: 'N', 5: '<Reserved>',
+               6: 'R', 7: 'S', 8: 'B', 9: 'C',
+               10: '<Reserved>', 11: '<Reserved>'}
 
-tc_type = {0: 'J', 1: 'K', 2: 'T',
-           3: 'E', 4: 'N', 5: '<Reserved>',
-           6: 'R', 7: 'S', 8: 'B', 9: 'C',
-           10: '<Reserved>', 11: '<Reserved>'}
+    output_hw_type = {0x00: 'None', 0x01: 'Single Pole', 0x02: 'Solid State Relay', 0x04: 'Double Pole',
+                      0x08: 'DC Pulse', 0x10: 'Analog', 0x20: 'Isolated Analog', 0x40: 'Isolated DC Pulse'}
 
-output_hw_type = {0x00: 'None', 0x01: 'Single Pole', 0x02: 'Solid State Relay', 0x04: 'Double Pole',
-                  0x08: 'DC Pulse', 0x10: 'Analog', 0x20: 'Isolated Analog', 0x20: 'Isolated DC Pulse'}
+    output_polarity = {0: 'Normally Open', 1: 'Normally Closed'}
 
-output_polarity = {0: 'Normally Open', 1: 'Normally Closed'}
+    output_type = {0: 'Voltage', 1: 'Current'}
 
-output_type = {0: 'Voltage', 1: 'Current'}
+    output_mode = {0: 'Off', 1: 'PID', 2: 'On/Off',
+                   3: 'Retransmission', 4: 'Alarm 1',
+                   5: 'Alarm 2', 6: 'Ramp Event',
+                   7: 'Soak Event', }
+
+    output_process_range = {0: '0-10 Vdc', 1: '0-5 Vdc', 2: '0-20 mA',
+                            3: '4-20 mA', 4: '0-24 mA', }
+
+
+class write():
+    control = {v: k for k, v in read.control.items()}
+
+    control_action = {v: k for k, v in read.control_action.items()}
+
+    system_state = {v: k for k, v in read.system_state.items()}
+
+    ramp_soak_tracking = {v: k for k, v in read.ramp_soak_tracking.items()}
+
+    sensor_type = {v: k for k, v in read.sensor_type.items()}
+
+    tc_type = {v: k for k, v in read.tc_type.items()}
+
+    output_hw_type = {v: k for k, v in read.output_hw_type.items()}
+
+    output_polarity = {v: k for k, v in read.output_polarity.items()}
+
+    output_type = {v: k for k, v in read.output_type.items()}
+
+    output_mode = {v: k for k, v in read.output_mode.items()}
+
+    output_process_range = {v: k for k, v in read.output_process_range.items()}
+
+
+class readonly():
+    readonly_ramp_soak_sate = {0x00: 'Inactive', 0x01: 'Ramping', 0x02: 'Soaking', 0x04: 'Ramp Active',
+                               0x08: 'Soak Active', 0x05: 'Ramping, Ramp Active', 0x0a: 'Soaking, Soak Active',
+                               0x10: 'Ramp Soak Paused', 0x80: 'Ramp Soak Error'}
+
+class register_map():
+    def get_output_register(self, output: int, datum: str):
+        output_registers_start = {1: 1024, 2: 1056, 3: 1088, 4: 1120,
+                                  5: 1152, 6: 1184, 7: 1216, 8: 1248}
+        output_registers_offset = {"hw_type":  0,
+                                   'mode': 1,
+                                   'on_off_action': 2,
+                                   'setpoint': 3,
+                                   'pulse_length': 4,
+                                   'on_off_deadband': 6,
+                                   'output_range': 8,
+                                   'retran_reading1': 10,
+                                   'retran_output1': 12,
+                                   'retran_reading2': 14,
+                                   'retran_output2': 16}
+
+        if datum not in output_registers_offset:
+            raise ValueError("Datum label supplied ({}) is not available for controller ouputs. "
+                             "Valid data labels {}".format(datum, output_registers_offset.keys()))
+        return output_registers_start[output] + output_registers_offset[datum]
+
+
+
+
+
+
