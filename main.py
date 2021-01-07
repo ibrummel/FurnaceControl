23bae64883d4eadb5fab80471334c162f1026530
Ian B <ian.brummel@gmail.com>
2021-01-07 11:23:38 -0500

    Updated to pull UI from python version of UI file.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PyQt5.QtCore import QThread, pyqtSignal
 import Omega_Platinum_Enum as ENUM
 import nidaqmx
 import numpy as np
+from src.ui.main_ui import Ui_Dialog
 import sys
 from Omega_Platinum import OmegaPlatinumControllerModbus
 
@@ -39,50 +40,44 @@ class FurnaceLogger(QDialog):
 
         self.external_tc = self.init_external_tc(tcdevpath)
 
-        self.ui = uic.loadUi("./src/ui/main.ui", self)
+        # self.ui = uic.loadUi("./src/ui/main.ui", self)
+        self.ui = Ui_Dialog()
+        self.ui.setupUi()
 
         self.data_thread = QThread()
-
-        # FIXME: Uncomment these while writing code, provides useful intellisense,
-        #  but will break the actual UI if left uncommented on run. 
-        # self.combo_monitor_tc = QComboBox()
-        # self.combo_controller_tc = QComboBox()
-        # self.combo_output_mode = QComboBox()
-        # self.combo_profile_tracking = QComboBox()
-        # self.combo_profile_number = QComboBox()
 
         self.init_fields()
         self.init_connections()
 
     def init_fields(self):
-        self.combo_monitor_tc.addItems(list(self.tc_types.keys()))
-        self.combo_controller_tc.addItems(['K', 'J', 'T', 'E', 'N', 'L', 'R', 'S', 'B', 'C'])
-        self.combo_output_mode.addItems(list(ENUM.write.output_mode.keys()))
-        self.combo_profile_number.addItems([str(x) for x in np.linspace(1, 99, 99)])
-        self.combo_profile_tracking.addItems(list(ENUM.write.ramp_soak_tracking.keys()))
+        self.ui.combo_monitor_tc.addItems(list(self.tc_types.keys()))
+        self.ui.combo_controller_tc.addItems(['K', 'J', 'T', 'E', 'N', 'L', 'R', 'S', 'B', 'C'])
+        self.ui.combo_output_mode.addItems(list(ENUM.write.output_mode.keys()))
+        self.ui.combo_profile_number.addItems([str(x) for x in np.linspace(1, 99, 99)])
+        self.ui.combo_profile_tracking.addItems(list(ENUM.write.ramp_soak_tracking.keys()))
         self.update_fields()
 
     def update_fields(self):
-        if not self.combo_monitor_tc.hasFocus():
-            self.combo_monitor_tc.setCurrentText(self.external_tc.ai_thrmcpl_type.name)
+        if not self.ui.combo_monitor_tc.hasFocus():
+            self.ui.combo_monitor_tc.setCurrentText(self.external_tc.ai_thrmcpl_type.name)
 
-        if not self.combo_controller_tc.hasFocus():
+        if not self.ui.combo_controller_tc.hasFocus():
             tc_type = self.controller.get_tc_type()
             if tc_type == '<Reserved>':
                 raise ValueError('Received invalid enumeration of thermocouple type from furnace controller.')
-            self.combo_controller_tc.setCurrentText(tc_type)
+            self.ui.combo_controller_tc.setCurrentText(tc_type)
 
-        if not self.combo_output_mode.hasFocus():
-            self.combo_output_mode.setCurrentText(self.controller.get_output_mode(3))
+        if not self.ui.combo_output_mode.hasFocus():
+            self.ui.combo_output_mode.setCurrentText(self.controller.get_output_mode(3))
 
-        if not self.combo_profile_number.hasFocus():
-            self.combo_profile_number.setCurrentText(str(self.controller.get_current_profile_number()))
+        if not self.ui.combo_profile_number.hasFocus():
+            self.ui.combo_profile_number.setCurrentText(str(self.controller.get_current_profile_number()))
 
-        if not self.combo_profile_tracking.hasFocus():
-            self.combo_profile_tracking.setCurrentText(self.controller.get_ramp_soak_tracking_mode())
+        if not self.ui.combo_profile_tracking.hasFocus():
+            self.ui.combo_profile_tracking.setCurrentText(self.controller.get_ramp_soak_tracking_mode())
 
     def init_connections(self):
-        self.combo_monitor_tc.currentTextChanged.connect(self.set_external_tc_type)
+        self.ui.combo_monitor_tc.currentTextChanged.connect(self.set_external_tc_type)
         #FIXME: Finish out connections to other controls
 
     def init_external_tc(self, tcdevpath):
